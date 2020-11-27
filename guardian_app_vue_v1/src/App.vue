d6de28efd3e0a41a75275fec647757ed58fff3ac
Ian F <ian.fiddeman@gmail.com>
2020-11-27 22:55:02 +0000

    api working with pages

---
+++ guardian_app_vue_v1/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1>Test</h1>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return {
+      stuff: [],
+
+    }
+  },  
+    mounted(){
+      this.fetchdata()
+    },
+
+  methods: {
+    fetchdata(){
+    fetch("http://content.guardianapis.com/search?page=1&page-size=150&q=peter%20bradshaw&format=json&tag=film/film,tone/reviews&from-date=2010-01-01&order-by=newest&show-blocks=all&api-key=test")
+    .then(response => response.json())
+    .then(data => this.stuff = data.response.results)
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
