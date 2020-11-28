285f83e6750609180667d0e5adefa3c82001a155
Ian F <ian.fiddeman@gmail.com>
2020-11-28 13:47:42 +0000

    promise working with single page

--- guardian_app_vue_v1/src/App.vue
+++ guardian_app_vue_v1/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Test</h1>
+  <p>{{this.seedata}}</p>
 </template>
 
 <script>
@@ -8,21 +8,55 @@ export default {
   data(){
     return {
       stuff: [],
+      seedata: [],
 
     }
   },  
     mounted(){
-      this.fetchdata()
+      this.fetchdata();
     },
 
-  methods: {
-    fetchdata(){
-    fetch("http://content.guardianapis.com/search?page=1&page-size=150&q=peter%20bradshaw&format=json&tag=film/film,tone/reviews&from-date=2010-01-01&order-by=newest&show-blocks=all&api-key=test")
-    .then(response => response.json())
-    .then(data => this.stuff = data.response.results)
-    },
+    // methods: {
+    //   fetchdata(){
+    //   fetch("http://content.guardianapis.com/search?page=1&page-size=150&q=peter%20bradshaw&format=json&tag=film/film,tone/reviews&from-date=2010-01-01&order-by=newest&show-blocks=all&api-key=test")
+    //   .then(response => response.json())
+    //   .then(data => this.stuff = data.response.results)
+    //   },
+    // }
+
+    methods: {
+      fetchdata(){
+        const promises = [1].map(num => {
+        return fetch("http://content.guardianapis.com/search?page=1&page-size=150&q=peter%20bradshaw&format=json&tag=film/film,tone/reviews&from-date=2010-01-01&order-by=newest&show-blocks=all&api-key=test"
+        ).then(res => res.json());
+        });
+        
+        Promise.all(promises)
+          .then(data => {
+            let reviewData = data.map (article => article.response.results);
+            console.log(reviewData)
+            // reviewData.flat(2);
+            
+            // reviewData.reduce(
+            //   (flat, toFlatten) => flat.concat(toFlatten),
+            //   []
+            // );
+            // console.log(data);
+            console.log(reviewData.flat(3))
+            this.seedata = [...reviewData.flat(3)];
+          })
+       
+  
+      }
+    }
+
+
   }
-}
+    //   // get MVP working without promise, then come back to it later. 
+     
+        // }
+        // .then(reviewData => this.stuff = data.response.results);
+
 </script>
 
 <style>
